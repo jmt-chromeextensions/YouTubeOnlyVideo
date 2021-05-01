b98043f74c13c5e4410baf126dddd22e2ded640d
JMartinTorres <jmartintorres@outlook.es>
2021-05-01 14:30:29 +0200

    Commented initialize_extension.js

--- youtube_only_video/background_scripts/initialize_extension.js
+++ youtube_only_video/background_scripts/initialize_extension.js
@@ -1,29 +1,29 @@
-function executeScripts(tabId, scripts)
-{
-    function createCallback(i) {
-        if (i < scripts.length -1)
-            return chrome.tabs.executeScript(tabId, { file: scripts[i]} , function() { createCallback(i + 1) } );
+// function executeScripts(tabId, scripts)
+// {
+//     function createCallback(i) {
+//         if (i < scripts.length -1)
+//             return chrome.tabs.executeScript(tabId, { file: scripts[i]} , function() { createCallback(i + 1) } );
         
-        return chrome.tabs.executeScript(tabId, { file: scripts[i]}, null);
-    }
+//         return chrome.tabs.executeScript(tabId, { file: scripts[i]}, null);
+//     }
 
-    chrome.tabs.executeScript(tabId, { file: scripts[0]} , function() { createCallback(1) } );
+//     chrome.tabs.executeScript(tabId, { file: scripts[0]} , function() { createCallback(1) } );
 
-}
+// }
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    debugger;
+// chrome.runtime.onInstalled.addListener(function (details) {
+//     debugger;
     
-    if (details.reason === "install") {
-        console.log("First installation. Executing content script in every open tab.")
-        chrome.tabs.query({ url: ["http://*/*", "https://*/*"] }, function (tabs) {
-            console.log(tabs);
-            for (var i = 0, length = tabs.length; i < length; i++) {
-                executeScripts(tabs[i].id, ["jquery/jquery-3.4.1.min.js", "jquery/jquery-ui.min.js",
-                                            "tools/color-thief/color-thief.min.js", "tools/jbox/jBox.all.min.js", "tools/spectrum/spectrum.js",
-                                             "icons/buttons_icons.js", "content_scripts/youtube_only_video.js"]);
-            }
-        });
-    }
+//     if (details.reason === "install") {
+//         console.log("First installation. Executing content script in every open tab.")
+//         chrome.tabs.query({ url: ["http://*/*", "https://*/*"] }, function (tabs) {
+//             console.log(tabs);
+//             for (var i = 0, length = tabs.length; i < length; i++) {
+//                 executeScripts(tabs[i].id, ["jquery/jquery-3.4.1.min.js", "jquery/jquery-ui.min.js",
+//                                             "tools/color-thief/color-thief.min.js", "tools/jbox/jBox.all.min.js", "tools/spectrum/spectrum.js",
+//                                              "icons/buttons_icons.js", "content_scripts/youtube_only_video.js"]);
+//             }
+//         });
+//     }
 
-});
+// });
